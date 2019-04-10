6165887dce7469fc870f9e8a82a50fe73ad09f75
Easy-Zhou <401700108@qq.com>
2019-04-10 13:58:09 +0800

    download v2 新增了对网页解码的功能

--- Learn/downloadweb.py
+++ Learn/downloadweb.py
@@ -9,27 +9,37 @@ import urllib.request
 from urllib.error import URLError, HTTPError, ContentTooShortError
 
 
-def download(url, user_agent='wswp', num_retries=2):
+def download(url, user_agent='wswp', num_retries=2, charset='utf-8'):
     """
     下载网页
     :param url: 网页地址
     :param user_agent: 用户代理 default wswp
     :param num_retries: 重试下载次数 default 2
+    :param charset: 网页编码方式 default utf-8
     :return: 抓取的网页html
     """
     print('Downloading:', url)
     # ---添加用户代理--- #
     request = urllib.request.Request(url)
     request.add_header('User-agent', user_agent)
+    # ---添加用户代理--- #
     # ---捕获网页不存在等一些异常--- #
     try:
-        html = urllib.request.urlopen(request).read()
+        # ---将网页进行解码--- #
+        resp = urllib.request.urlopen(request)
+        cs = resp.headers.get_content_charset()
+        if not cs:
+            print("use default charset utf-8")
+            cs = charset
+        # ---将网页进行解码--- #
+        html = resp.read().decode(cs)
     except (URLError, HTTPError, ContentTooShortError) as e:
         print('Download error:', e.reason)
         html = None
         if num_retries > 0:
             if hasattr(e, 'code') and 500 <= e.code < 600:
                 return download(url, num_retries=num_retries - 1)
+    # ---捕获网页不存在等一些异常--- #
     return html
 
 
